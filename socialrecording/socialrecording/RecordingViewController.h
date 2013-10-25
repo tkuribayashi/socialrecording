8ab0b00a7b9879b12b73de8125ac95563f989489
taku <choco.ms@gmail.com>
2013-10-25 19:17:11 +0900

    録音と再生を実装。サーバへの送信はまだです。

--- socialrecording/socialrecording/RecordingViewController.h
+++ socialrecording/socialrecording/RecordingViewController.h
@@ -7,8 +7,12 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
 
 @interface RecordingViewController : UIViewController
+@property (nonatomic)AVAudioRecorder *recorder;
+@property (nonatomic)AVAudioSession *session;
+@property (nonatomic)AVAudioPlayer *player;
 - (IBAction)button_record_tapped:(id)sender;
 - (IBAction)button_play_tapped:(id)sender;
 - (IBAction)button_send_tapped:(id)sender;
