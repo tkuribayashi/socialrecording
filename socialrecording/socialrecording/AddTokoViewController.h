319c31f55998d96551022ec7c43444eb47f8fe6e
monouru <tkytango@gmail.com>
2013-11-25 03:00:00 +0900

    storyboard最後にちょっといじり

--- socialrecording/socialrecording/AddTokoViewController.h
+++ socialrecording/socialrecording/AddTokoViewController.h
@@ -10,6 +10,7 @@
 #import "UIToggleButton.h"
 
 @interface AddTokoViewController : UIViewController<UITextViewDelegate>
+@property (weak, nonatomic) IBOutlet UILabel *label_name;
 @property (weak, nonatomic) IBOutlet UITextField *text_name;
 @property (weak, nonatomic) IBOutlet UIToggleButton *toggle_button_1;
 @property (weak, nonatomic) IBOutlet UIToggleButton *toggle_button_2;
@@ -21,6 +22,7 @@
 - (IBAction)button_add_tapped:(id)sender;
 - (IBAction)text_name_end:(id)sender;
 @property (nonatomic) UIButton *button_edit_end;
+@property (weak, nonatomic) IBOutlet UILabel *label_comment;
 @property (weak, nonatomic) IBOutlet UITextView *text_comment;
 @property (nonatomic)BOOL flag_complete;
 @end
